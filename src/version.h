19d52818f34ad6e938e4bf355e85aebd74396a39
John Regan <john@jrjrtech.com>
2023-08-13 08:44:36 -0400

    version 1.0.2

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 
 #define ICECAST_HLS_VERSION_MAJOR 1
 #define ICECAST_HLS_VERSION_MINOR 0
-#define ICECAST_HLS_VERSION_PATCH 1
+#define ICECAST_HLS_VERSION_PATCH 2
 
 #ifdef __cplusplus
 extern "C" {
